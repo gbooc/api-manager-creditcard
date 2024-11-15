c20d6fecdfdb1d1a866d5d95640e4924cdd49799
gbooc <gbooc23@gmail.com>
2024-11-15 13:32:41 +0800

    First project creation
    coding challenge - day 1

---
+++ Controllers/CreditCardController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api_manager_creditcard.Controllers
+{
+    public class CreditCardController : ApiController
+    {
+        public HttpResponseMessage Post([FromBody] string creditcardnumber)
+        {
+            creditcardnumber = creditcardnumber.Replace(" ", string.Empty); // Remove spaces
+
+            //Return false if empty
+            if (String.IsNullOrEmpty(creditcardnumber) || creditcardnumber.Length < 15)
+                return Request.CreateResponse(HttpStatusCode.Created, false);
+
+            //Start of luhn algorithm implementation
+            bool alternate = false;
+            int sum = 0;
+
+            // Loop through the digits in reverse order
+            for (int i = creditcardnumber.Length - 1; i >= 0; i--)
+            {
+                if (!char.IsDigit(creditcardnumber[i]))
+                    return Request.CreateResponse(HttpStatusCode.Created, false); ; // Return false if non numeric char found
+
+                int digit = creditcardnumber[i] - '0'; // Convert the char to an integer
+
+                if (alternate)
+                {
+                    // Double every other digit
+                    digit *= 2;
+                    if (digit > 9)
+                        digit -= 9; // Subtract 9 if the doubled value is greater than 9
+                }
+                sum += digit;
+                alternate = !alternate; // Toggle alternate flag
+            }
+
+            // If the sum is divisible by 10, the number is valid
+            return Request.CreateResponse(HttpStatusCode.Created, (sum % 10 == 0 ? true : false));
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="api_manager_creditcard.Global" Language="C#" %>

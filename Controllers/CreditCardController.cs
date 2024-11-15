a03512922c3de85c5a78d547f2a268514fc14c96
gbooc <gbooc23@gmail.com>
2024-11-15 14:04:22 +0800

    added error handling

--- Controllers/CreditCardController.cs
+++ Controllers/CreditCardController.cs
@@ -11,38 +11,44 @@ namespace api_manager_creditcard.Controllers
     {
         public HttpResponseMessage Post([FromBody] string creditcardnumber)
         {
-            creditcardnumber = creditcardnumber.Replace(" ", string.Empty); // Remove spaces
-
-            //Return false if empty
-            if (String.IsNullOrEmpty(creditcardnumber) || creditcardnumber.Length < 15)
-                return Request.CreateResponse(HttpStatusCode.Created, false);
-
-            //Start of luhn algorithm implementation
-            bool alternate = false;
-            int sum = 0;
-
-            // Loop through the digits in reverse order
-            for (int i = creditcardnumber.Length - 1; i >= 0; i--)
+            try
             {
-                if (!char.IsDigit(creditcardnumber[i]))
-                    return Request.CreateResponse(HttpStatusCode.Created, false); ; // Return false if non numeric char found
+                creditcardnumber = creditcardnumber.Replace(" ", string.Empty); // Remove spaces
 
-                int digit = creditcardnumber[i] - '0'; // Convert the char to an integer
+                //Return false if empty and incorrect format for cc numbers
+                if (String.IsNullOrEmpty(creditcardnumber) || creditcardnumber.Length < 15)
+                    return Request.CreateResponse(HttpStatusCode.Created, false);
 
-                if (alternate)
+                //Start of luhn algorithm implementation
+                bool alternate = false;
+                int sum = 0;
+
+                // Loop through the digits in reverse order
+                for (int i = creditcardnumber.Length - 1; i >= 0; i--)
                 {
-                    // Double every other digit
-                    digit *= 2;
-                    if (digit > 9)
-                        digit -= 9; // Subtract 9 if the doubled value is greater than 9
+                    if (!char.IsDigit(creditcardnumber[i]))
+                        return Request.CreateResponse(HttpStatusCode.Created, false); // Return false if non numeric char found
+
+                    int digit = creditcardnumber[i] - '0'; // Convert the char to an integer
+
+                    if (alternate)
+                    {
+                        // Double every other digit
+                        digit *= 2;
+                        if (digit > 9)
+                            digit -= 9; // Subtract 9 if the doubled value is greater than 9
+                    }
+                    sum += digit;
+                    alternate = !alternate;
                 }
-                sum += digit;
-                alternate = !alternate; // Toggle alternate flag
+
+                // If the sum is divisible by 10, the number is valid
+                return Request.CreateResponse(HttpStatusCode.Created, (sum % 10 == 0 ? true : false));
             }
-
-            // If the sum is divisible by 10, the number is valid
-            return Request.CreateResponse(HttpStatusCode.Created, (sum % 10 == 0 ? true : false));
-
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(ex);
+            }
         }
     }
 }
